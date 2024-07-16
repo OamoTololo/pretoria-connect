0db6e90e9e34b6836d71fc90e2dd5c5f6125e4b6
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-07-16 18:23:28 +0200

    Type hinted actions for automatic fetching of entities using param converter.

--- src/Controller/VlogController.php
+++ src/Controller/VlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,22 +35,24 @@ class VlogController extends AbstractController
 
     /**
      * @Route("/post/{id}", name="vlog_by_id", requirements={"id"="\d+"})
+     * @ParamConverter("post", class="App\Entity\VlogPost")
      */
-    public function post($id)
+    public function post(VlogPost $post)
     {
-        return $this->json($this->getDoctrine()->getRepository(VlogPost::class)->find($id));
+        return $this->json($post);
     }
 
     /**
      * @Route("/post/{slug}", name="vlog_by_slug")
+     * @ParamConverter("post", class="App\Entity\VlogPost", options={"mapping": {"slug": "slug"}})
      */
-    public function postBySlug($slug)
+    public function postBySlug(VlogPost $post)
     {
-        return $this->json($this->getDoctrine()->getRepository(VlogPost::class)->findOneBy(['slug' => $slug]));
+        return $this->json($post);
     }
 
     /**
-     * @Route("/add", name="add_vlog", methods={"GET"})
+     * @Route("/add", name="add_vlog", methods={"POST"})
      */
     public function add(Request $request)
     {
