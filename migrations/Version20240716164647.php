b4355ed18f8347a17d2dc3a69abf10cdc5c297ba
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-07-16 18:48:50 +0200

    Created a method to add fake data into the vlog posts table.

--- migrations/Version20240716164647.php
+++ migrations/Version20240716164647.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240716153539 extends AbstractMigration
+final class Version20240716164647 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\VlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Factory
+     */
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadVlogPosts($manager);
+    }
 
+    public function loadVlogPosts(ObjectManager $manager): void
+    {
+        $vlogPosts = new VlogPost();
+        $vlogPosts->setTitle($this->faker->realText(30));
+        $vlogPosts->setPublish($this->faker->dateTimeThisYear);
+        $vlogPosts->setContent($this->faker->realText());
+        $vlogPosts->setAuthor($this->faker->name);
+        $vlogPosts->setSlug($this->faker->slug);
+
+        $manager->persist($vlogPosts);
         $manager->flush();
     }
 }
